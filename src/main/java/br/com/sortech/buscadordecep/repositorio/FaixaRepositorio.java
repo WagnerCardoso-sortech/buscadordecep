5b9293e061d97973a0dca09690a8c22da2eb6925
WagnerCarsoso-sortech <wagner.cardoso@sortech.com.br>
2022-07-22 11:45:33 -0300

    atualizaçao

--- src/main/java/br/com/sortech/buscadordecep/repositorio/FaixaRepositorio.java
+++ src/main/java/br/com/sortech/buscadordecep/repositorio/FaixaRepositorio.java
@@ -1,6 +1,5 @@
 package br.com.sortech.buscadordecep.repositorio;
 
-import br.com.sortech.buscadordecep.dominio.Cep;
 import br.com.sortech.buscadordecep.dominio.Faixa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
